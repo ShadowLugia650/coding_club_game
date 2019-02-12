19664b394cdf746aad35c124ba387d50fe23c611
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-02-11 21:13:50 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys, random
+import sys, random, discord
+from discord.ext import commands
+from discord.ext.commands import Bot
 
 sys.path.insert(0, 'dependencies')
 import console, pScript, baseM, itemStats
@@ -15,10 +17,24 @@ hard= [owM, Collector, demonicWarrior]
 sys.path.insert(0, 'Rooms/Impossible')
 impossible = []
 
+Client = discord.Client()
+client = commands.Bot(command_prefix=commands.when_mentioned_or('>'))
 Player = pScript.PChar()
 levels = easy
 consol = ""
 rounds = 0
+
+@client.command(pass_context=True)
+async def cont(ctx):
+    """Continue the story"""
+    
+@client.command(pass_context=True)
+async def view(ctx, *, item):
+    """View an item and its description"""
+    itm = eval(baseM.strToClsNm(item))()
+    if itm is not None:
+        await client.say("Description:\n{}".format(itm.desc))
+        #await client.send_file(ctx.message.channel, "resources/{}.png".format(itm.name)) #sends picture
 
 baseM.initIntro(Player)
 while Player.alive:
