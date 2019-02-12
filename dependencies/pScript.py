225004f14f107cf6009462c0dcfa3114d1beb769
TofuGroves <40476486+TofuGroves@users.noreply.github.com>
2019-02-11 19:09:51 -0800

    Update pScript.py

--- dependencies/pScript.py
+++ dependencies/pScript.py
@@ -8,6 +8,7 @@ class PChar(): #intializes the class. The Main handles this, so you don't need i
         self.alive = True
         self.damage = 3
         self.block = 3
+        self.maxHp = 100
 
 def checkDeath(Player): #checks if the player is dead. Handled automatically in the damage() function.
     if Player.health <= 0:
