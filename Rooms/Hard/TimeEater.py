e36142befbdb030f57808862ab9712cdfc0a31ba
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-02-16 19:04:31 -0800

    Update TimeEater.py

--- Rooms/Hard/TimeEater.py
+++ Rooms/Hard/TimeEater.py
@@ -2,6 +2,22 @@ import sys
 sys.path.insert(0, 'dependencies')
 import baseM
 
+class timeEater(baseM.basicEnemy):
+    def __init__(self):
+        self.type = "Time Eater"
+        self.baseDamage = 10
+        self.health = 250
+        self.maxHp = 250
+        self.loot = [("Gold", random.randint(100,150))]#, HungryClock()]
+        self.options = {"Consume":0, ""}
+        
+    def move(self):
+        
+
 def run(player):
-    print()
+    enemies = [timeEater()]
+    timeEater.baseDamage *= player.timeClimbing
+    print("You walk into a large spacious room. There are clocks all over the walls, each with a different name on it.")
+    print("As you continue walking you see your name. Suddenly, the clock starts to grow, and the hands reach out toward you.")
+    
     return player
