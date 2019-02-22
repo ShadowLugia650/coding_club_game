556592d4d23ba40af635e18dc654ef1b8afb14cb
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-02-22 11:58:14 -0800

    Update TimeEater.py

--- Rooms/Impossible/TimeEater.py
+++ Rooms/Impossible/TimeEater.py
@@ -59,11 +59,10 @@ class timeEater(baseM.basicEnemy):
 
 def run(player):
     t = timeEater()
-    enemies = [t]
     if len(player.items) > 50:
         t.baseDamage = 15
     t.baseDamage *= player.timeClimbing
     print("You walk into a large spacious room. There are clocks all over the walls, each with a different name on it.")
     print("As you continue walking you see your name. Suddenly, the clock starts to grow, and the hands reach out toward you.")
-    baseM.runBasicFight(player, enemies)
+    baseM.runBasicFight(player, [t])
     return player
