4464a022b49487772925ee8d0375b5b76fe10904
TofuGroves <40476486+TofuGroves@users.noreply.github.com>
2019-02-23 13:29:40 -0800

    Update curseScript.py

--- dependencies/curseScript.py
+++ dependencies/curseScript.py
@@ -1,2 +1,55 @@
 #this script is for curses, which are player debuffs that last until dispelled by special effects.
 
+import random, itemStats
+
+class basicCurse():
+    def __init__(self):
+        self.name = None
+        self.desc = ""
+        self.effectText = ""
+        self.target = None
+
+    def __str__(self):
+        return self.name
+    
+    def readDesc(self):
+        print(self.desc)
+
+    def onFloorClimb(self, player):
+        pass
+
+    def onCombatTurn(self, player):
+        pass
+
+    def printEffect(self):
+        return self.effectText
+    
+    def curseItem(self, item):
+        self.target = item
+
+class exhaustion(basicCurse):
+    def __init__(self):
+        self.severity = 1
+        self.name = "Exhaustion level " + str(self.severity)
+        self.desc = "Mental and physical fatigue from lack of sleep."
+
+    def onCombatTurn(self, player):
+        choice = random.randint(1, 2)
+        fatigue = random.randint(1, self.severity)
+        if choice == 1:
+            if player.damage > fatigue:
+                player.damage -= fatigue
+            else:
+                player.damage = 1
+            self.effectText = "Exhaustion saps your strength!"
+        else:
+            if player.block > fatigue:
+                player.block -= fatigue
+            else:
+                player.block = 1
+            self.effectText = "Exhaustion lowers your dexterity!"
+        printEffect(self)
+
+    def onFloorClimb(self, player):
+        self.severity += 1
+
