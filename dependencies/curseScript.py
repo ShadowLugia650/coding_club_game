f7f13eb8424d303831281657fbda2140b0fc8c45
TofuGroves <40476486+TofuGroves@users.noreply.github.com>
2019-02-23 12:43:43 -0800

    Update curseScript.py

--- dependencies/curseScript.py
+++ dependencies/curseScript.py
@@ -1,2 +1,2 @@
-*this script is for curses, which are player debuffs that last until dispelled by special effects.
+#this script is for curses, which are player debuffs that last until dispelled by special effects.
 
