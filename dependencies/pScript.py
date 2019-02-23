cf6efa442681919ee9b96ce5dccfec7749ac91b6
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-02-22 17:29:16 -0800

    Update pScript.py

--- dependencies/pScript.py
+++ dependencies/pScript.py
@@ -9,8 +9,11 @@ class PChar(): #intializes the class. The Main handles this, so you don't need i
         self.damage = 3
         self.block = 3
         self.maxHp = 100
+        #These are non-main attributes of the player.
         self.timeClimbing = 0
+        self.impossible = False
 
+#I'd recommend having these as methods inside the player class so they're easier to use.
 def checkDeath(Player): #checks if the player is dead. Handled automatically in the damage() function.
     if Player.health <= 0:
         death(Player)
