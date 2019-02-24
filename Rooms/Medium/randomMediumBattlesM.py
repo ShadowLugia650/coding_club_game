15769a9e8467d388fcf1ee9ed60458cfa74bf89a
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-02-23 18:35:48 -0800

    Update randomMediumBattlesM.py

--- Rooms/Medium/randomMediumBattlesM.py
+++ Rooms/Medium/randomMediumBattlesM.py
@@ -1,6 +1,7 @@
 import sys, random
 sys.path.insert(0, 'dependencies')
 import baseM, itemStats
+from curseScript import toxins
 
 class Grenlim(baseM.basicEnemy):
     def __init__(self):
@@ -11,9 +12,26 @@ class Grenlim(baseM.basicEnemy):
         self.maxHp = 35
         self.loot = [("Gold", random.randint(30,50))]
         self.options = {"Slash":2, "Strike":0}
-
+        
+class Snak(baseM.basicEnemy):
+    def __init__(self):
+        self.type = "Snak"
+        self.baseDamage = 18
+        self.health = 20
+        self.maxHp = 20
+        self.loot = [("Gold", random.randint(30,50))]
+        self.options = {"Dry Strike":2, "Venomous Bite":-3}
+        
+    def move(self, player):
+        atk = random.choice(self.options.keys())
+        if atk == "Venomous Bite":
+            v = toxins()
+            v.severity = 3
+            player.curses.append(v)
+        return atk, self.options[atk]+self.baseDamage
+        
 def run(player):
-    enemies = [random.choice([Grenlim()])]
+    enemies = [random.choice([Grenlim(), Snak()])]
     print("You encountered a {}!".format(enemies[0].type))
     choice = input("What do you do? [Fight, Run]\n")
     if choice.title() in ["Fight", "F", "Attack"]:
@@ -21,6 +39,9 @@ def run(player):
         baseM.runBasicFight(player, enemies)
     elif choice.title() in ["Flee", "Run", "R"]:
         print("You run away from the {} and it deals {} to you.".format(enemies[0].type, enemies[0].baseDamage))
+        if type(enemies[0]) == Snak:
+            print("The Snak poisoned you with its bite!")
+            player.curses.append(toxins())
         player.health -= enemies[0].baseDamage
     else:
         baseM.checkCommands(choice, player)
