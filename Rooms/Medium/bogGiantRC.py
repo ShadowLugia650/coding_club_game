c29d2b059df6b4e6d38d733014d4294e09dc1832
Ozymandias-KingofKings <47375293+Ozymandias-KingofKings@users.noreply.github.com>
2019-02-25 14:19:09 -0800

    Add files via upload

---
+++ Rooms/Medium/bogGiantRC.py
@@ -0,0 +1,32 @@
+import baseM
+import itemStats
+import console
+import curseScript
+class BogGiant(baseM.basicEnemy):
+    def __init__(self):
+        self.type = "Bog Giant"
+        self.baseDamage = 20
+        self.health = 200
+        self.maxHp = 200
+        self.loot=[itemStats.vileBlade()]
+        self.options = {"Stab":-5,"Slash":+10}
+def run(player):
+    print("""A gnarled tree trunk protrudes from the cracked tile of the floor.
+Sticking from the top of the tree trunk is a sword of dark grey steel
+How would you wish to proceed?
+Press 1 to take the sword
+Press 2 to leave""")
+    while True:
+        choice=input()
+        if choice=="1":
+            print("""You yank hard on the sword, trying to dislodge it.
+To your surprise, the floor begins to rumble and the tree trunk begins to rise.
+You realize that the tree trunk is actually the head of a massive bog giant!""")
+            baseM.runBasicFight(player, [BogGiant()], 0, True)
+            return player
+        elif choice=="2":
+            print ("You suspect a trap, so you leave the sword untouched.")
+            return player
+        else:
+            print("Sorry, that is not one of your options")
+        
