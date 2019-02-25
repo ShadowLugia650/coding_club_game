7ca937657acb84fa07a18c52155d4d5f7dde979e
Ozymandias-KingofKings <47375293+Ozymandias-KingofKings@users.noreply.github.com>
2019-02-25 14:26:59 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,50 +17,52 @@ hard= [owM, Collector, demonicWarrior]
 sys.path.insert(0, 'Rooms/Impossible')
 import TimeEater
 impossible = [TimeEater]
+def game():
+    Player = pScript.PChar()
+    levels = easy
+    consol = ""
+    rounds = 0
 
-Player = pScript.PChar()
-levels = easy
-consol = ""
-rounds = 0
-
-baseM.initIntro(Player)
-while Player.alive:
-    possibleLevels = levels
-    if Player.impossible:
-        possibleLevels += impossible
-    room = random.choice(possibleLevels)
-    consol = console.getInput(Player, ["yes", "no"], "next room? ")
-    if consol.lower() in ["yes", "y", "ok", "sure", "yeah", "yay"]:
-        room.run(Player)
-        rounds += 1
-        Player.timeClimbing += 2
-        for i in Player.items:
-            try:
-                if issubclass(i, itemStats.basicItem()):
-                    i.onFloorClimb(Player)
-            except TypeError:
-                pass
-        for i in Player.curses:
-            try:
-                if issubclass(i, curseScript.basicCurse()):
-                    i.onFloorClimb(Player)
-            except TypeError:
-                pass
-    else:
-        break
-    if Player.alive == False:
-        print("You have died.")
-    if rounds == 3:
-        levels += med
-    elif rounds == 20:
-        levels += hard
-print("Game over.")
-print("rooms cleared:  " + str(rounds))
-print("Ending gold:    " + str(Player.gold))
-print("Ending items:")
-for i in Player.items:
-    print("- {}".format(i))
-input()
+    baseM.initIntro(Player)
+    while Player.alive:
+        possibleLevels = levels
+        if Player.impossible:
+            possibleLevels += impossible
+        room = random.choice(possibleLevels)
+        consol = console.getInput(Player, ["yes", "no"], "next room? ")
+        if consol.lower() in ["yes", "y", "ok", "sure", "yeah", "yay"]:
+            room.run(Player)
+            rounds += 1
+            Player.timeClimbing += 2
+            for i in Player.items:
+                try:
+                    if issubclass(i, itemStats.basicItem()):
+                        i.onFloorClimb(Player)
+                except TypeError:
+                    pass
+            for i in Player.curses:
+                try:
+                    if issubclass(i, curseScript.basicCurse()):
+                        i.onFloorClimb(Player)
+                except TypeError:
+                    pass
+        else:
+            break
+        if Player.alive == False:
+            print("You have died.")
+        if rounds == 3:
+            levels += med
+        elif rounds == 20:
+            levels += hard
+    print("Game over.")
+    print("rooms cleared:  " + str(rounds))
+    print("Ending gold:    " + str(Player.gold))
+    print("Ending items:")
+    for i in Player.items:
+        print("- {}".format(i))
+    input()
+    game()
+game()
 
 
 
