a15b24ca80fcc583806a50d8209815914ce7a6df
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-03-01 09:20:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ sys.path.insert(0, 'Rooms/Easy')
 import shopM, battleZombomanM, bestRoomRC, GobolinRC, goldenTreeM
 easy= [shopM, battleZombomanM, bestRoomRC, GobolinRC, goldenTreeM]
 sys.path.insert(0, 'Rooms/Medium')
-import strangerJGv1, battleGhoulM, mirrorsM, lavaroomRCv1, skeletonCalvin, reverseRoomAT, jabberwockyJG, randomMediumBattlesM
-med = [strangerJGv1, battleGhoulM, mirrorsM, lavaroomRCv1, skeletonCalvin, reverseRoomAT]
+import strangerJGv1, battleGhoulM, mirrorsM, lavaroomRCv1, skeletonCalvin, reverseRoomAT, jabberwockyJG, randomMediumBattlesM, slimeroomCalvin
+med = [strangerJGv1, battleGhoulM, mirrorsM, lavaroomRCv1, skeletonCalvin, reverseRoomAT, jabberwockyJG]
 for i in range(3): #increase for each enemy added to randombattles
     med.append(randomMediumBattlesM)
 sys.path.insert(0, 'Rooms/Hard')
