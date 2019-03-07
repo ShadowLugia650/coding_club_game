d8e7fa52b691363edab1c0862537d0927beab037
Ozymandias-KingofKings <47375293+Ozymandias-KingofKings@users.noreply.github.com>
2019-03-07 14:50:07 -0800

    Update KillYourselfRC.py

--- Rooms/Impossible/KillYourselfRC.py
+++ Rooms/Impossible/KillYourselfRC.py
@@ -1,2 +1,17 @@
+import baseM
+import itemStats
+class Yourself(baseM.basicEnemy):
+    def __init__(self):
+        self.type = "Yourself"
+        self.baseDamage = 0
+        self.health = 200
+        self.maxHp = 200
+        self.loot=[itemStats.vileBlade()]
+        self.options = {"Stab":-5,"Slash":+10}
 def run(player):
+    you=Yourself()
+    you.baseDamage+=baseM.modifyPlayerEffects("atk", player)
+    you.health=player.health*3
+    you.maxHp=player.health*3
+    baseM.runBasicFight(player, [you], 0, True)
     
