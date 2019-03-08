e8488be976489726eedc915a1458f5c16b92f60e
TofuGroves <40476486+TofuGroves@users.noreply.github.com>
2019-03-08 15:47:23 -0800

    Add files via upload

---
+++ roomTester.py
@@ -0,0 +1,31 @@
+#This is a program that allows programmers to run thier rooms,
+#without monkeying around in Main.py. here, I am importing MoneyGremlim,
+#but feel free to import any room you want to test :)
+
+import sys, random
+
+sys.path.insert(0, 'dependencies')
+import console, pScript, baseM, itemStats
+
+sys.path.insert(0, 'Rooms/Easy')
+#import shopM, battleZombomanM, bestRoomRC, GobolinRC, goldenTreeM
+
+sys.path.insert(0, 'Rooms/Medium')
+#import strangerJGv1, battleGhoulM, mirrorsM, lavaroomRCv1, skeletonCalvin, reverseRoomAT, jabberwockyJG, randomMediumBattlesM, slimeroomCalvin
+
+sys.path.insert(0, 'Rooms/Hard')
+#import owM, Collector, demonicWarrior
+import moneyGremlim
+
+sys.path.insert(0, 'Rooms/Impossible')
+#import TimeEater
+
+
+
+Player = pScript.PChar()
+moneyGremlim.run(Player)
+
+if Player.alive == False:
+    print("You have died.")
+else:
+    print("This simulation has been TERMINATED."
