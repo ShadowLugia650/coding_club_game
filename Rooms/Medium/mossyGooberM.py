88a5e4dd04339feace85f7ae71ececc4bedcdbf1
ShadowLugia650 <43692481+ShadowLugia650@users.noreply.github.com>
2019-03-07 18:56:58 -0800

    Update mossyGooberM.py

--- Rooms/Medium/mossyGooberM.py
+++ Rooms/Medium/mossyGooberM.py
@@ -4,11 +4,18 @@ import baseM, curseScript, itemStats
 
 class MossyGoober(baseM.basicEnemy):
     def __init__(self):
-        self.type = "Mossy Giant"
+        self.type = "Mosstrosity"
         self.health = 85
-        self.baseDamage = 5
-        self.options = {}
-        self.loot = []
+        self.baseDamage = 10
+        self.options = {"Smack":10, "Colossal Punch":40}
+        self.loot = [("Gold", random.randint(10,20))]
+        self.turn = 0
+        
+    def move(self, player):
+        self.turn += 1
+        if self.turn == 1:
+            atk = 
+        atk = random.choice(list(self.options.keys()))
 
 def run(player):
     print("You encounter a thicket of vines preventing you from proceeding. What do you do?")
@@ -21,6 +28,9 @@ def run(player):
     elif choice.title() in ["Cut", "Vines", "Cut Vines", "C"]:
         if baseM.hasWeapon(player):
             print("As you carefully cut the vines you start to feel the ground move beneath you.")
+            print("You leap back onto solid ground as the giant green creature rises before you.")
+            print("The creature turns to you and massive, moss-covered arms move toward you.")
+            print("The enormous creature stands before you, and it looks like it's preparing to attack...")
         else:
             print("Without a proper weapon, you are unable to cut the vines.")
             run(player)
