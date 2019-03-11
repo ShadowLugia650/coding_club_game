8c27a22f493e25a188f56489ad4c97f86156c7e5
Ozymandias-KingofKings <47375293+Ozymandias-KingofKings@users.noreply.github.com>
2019-03-11 10:44:28 -0700

    Update ohgur.py

--- Rooms/Medium/ohgur.py
+++ Rooms/Medium/ohgur.py
@@ -5,11 +5,11 @@ import baseM, itemStats
 class Junior(baseM.basicEnemy):
     def __init__ (self):
         self.type="Ohgur"
-        self.baseDamage=17
+        self.baseDamage=3
         self.baseDef=2
         self.maxHp=70
         self.health=70
-        self.options = {"Kick":7, "Slash":10}
+        self.options = {"Kick":7, "Slash":12}
         self.loot=[itemStats.ohgurGuts(), itemStats.ohgurGuts()]
         
         
