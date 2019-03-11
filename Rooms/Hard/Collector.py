a18335ad0668d2b44fb6b558460d79b466729edc
TofuGroves <40476486+TofuGroves@users.noreply.github.com>
2019-03-11 11:36:29 -0700

    Update Collector.py

--- Rooms/Hard/Collector.py
+++ Rooms/Hard/Collector.py
@@ -10,7 +10,7 @@ class Collector(baseM.basicEnemy):
         self.block = 0
         self.health = 100
         self.maxHp = 100
-        self.loot = [("Gold",random.randint(90,110))]
+        self.loot = [("Gold", 114)]
         self.options = {"Stab":0,"Siphon":-3,"Slash":+1}
 def run(player):
     x=Collector()
