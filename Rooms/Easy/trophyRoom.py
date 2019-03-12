1d82c2519254bf25b78ac569b2085947152eea53
Ozymandias-KingofKings <47375293+Ozymandias-KingofKings@users.noreply.github.com>
2019-03-12 10:12:43 -0700

    Update trophyRoom.py

--- Rooms/Easy/trophyRoom.py
+++ Rooms/Easy/trophyRoom.py
@@ -4,11 +4,17 @@ def run(player):
     commonItems = [itemStats.shovel(), itemStats.healthPotion(), itemStats.brokenSword(), itemStats.dullKnife(), itemStats.rustyPitchfork(), itemStats.baseballBat(), itemStats.cardboardShield(), itemStats.tinyShield()]
     rareItems = [itemStats.staffOfGold(), itemStats.sword(), itemStats.shield(), itemStats.abandonedAxe(), itemStats.warhammer(), itemStats.diamondSword(), itemStats.glowingOrb()]
     legendaryItems = [itemStats.epiTome()]
-    print("The passageway opens up into a small, cramped, room lined with rusty weapons and tools.")
-    choice=input("Do you want to look around for something useful?")
     while True:
+        print("The passageway opens up into a small, cramped, room lined with rusty weapons and tools.")
+        choice=input("Do you want to look around for something useful?")
         if choice in ["yes", "y", "si", "of course"]:
             while True:
                 print("Grab many random items, or search carefully for specific useful items?")
                 choice = input("[Grab random, search for useful]")
-                
+                if choice.lower() in ["grab random", "grab", "random"]:
+                    
+                elif choice.lower() in ["search for useful","search", "useful"]:
+                    
+        elif choice in ["no", "n","never"]:
+            print("You exit the room.")
+            return player
