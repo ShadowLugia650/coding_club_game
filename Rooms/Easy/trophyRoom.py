9c15a2589a562160608d051cf3a6618fd45e3239
Ozymandias-KingofKings <47375293+Ozymandias-KingofKings@users.noreply.github.com>
2019-03-12 09:13:54 -0700

    Update trophyRoom.py

--- Rooms/Easy/trophyRoom.py
+++ Rooms/Easy/trophyRoom.py
@@ -1,2 +1,6 @@
 def run(player):
-    print("")
+    print("The passageway opens up into a small, cramped, room lined with rusty weapons and tools.")
+    choice=input("Do you want to look around for something useful?")
+    while True:
+        if choice in ["yes", "y", "si", "of course"]:
+            
